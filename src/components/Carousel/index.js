998cc5d8ac684068c543eb277d66e95e044af036
James Maupin <jmsMaupin1@yahoo.com>
2017-03-03 12:16:16 -0500

    Initial setup of the Carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import {Carousel} from 'react-bootstrap';
+import './carousel.css'
 
 export default class carousel extends Component {
-	render() {
-		return (
-			<div></div>
-		);
-	}
+  render() {
+    return (
+      <Carousel>
+        <Carousel.Item>
+          <div className="carousel-slide building">
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-slide couch">
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-slide clay-roof">
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
 }
--- src/views/App/index.js
+++ src/views/App/index.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Carousel />
+        <Carousel/>
       </div>
     );
   }
