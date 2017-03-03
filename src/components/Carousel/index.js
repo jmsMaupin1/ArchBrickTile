980ab4934f4238327b8ea916cc85aadd44c690c0
James Maupin <jmsMaupin1@yahoo.com>
2017-03-03 16:47:14 -0500

    Finish Initial design of Carousel component

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -5,19 +5,17 @@ import './carousel.css'
 export default class carousel extends Component {
   render() {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <div className="carousel-slide building">
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="carousel-slide couch">
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="carousel-slide clay-roof">
-          </div>
-        </Carousel.Item>
+      <Carousel interval={3000} pauseOnHover={false}>
+        {
+          this.props.slides.map((slide, index)=>{
+            return (
+              <Carousel.Item key={index}>
+                <div className="carousel-slide" style={{backgroundImage: 'url('+slide+')'}}>
+                </div>
+              </Carousel.Item>
+            );
+          })  
+        }
       </Carousel>
     );
   }
