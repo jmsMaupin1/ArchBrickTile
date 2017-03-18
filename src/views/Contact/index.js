d17628b8750ee3a01263b9b52568dfdbee4d9069
James Maupin <jmsMaupin1@yahoo.com>
2017-03-18 15:04:35 -0400

    Initial design of social links and contact form

--- src/views/Contact/index.js
+++ src/views/Contact/index.js
@@ -1,9 +1,85 @@
 import React, { Component } from 'react';
-
+import './contact.css';
 export default class Contact extends Component {
 	render() {
 		return (
-			<div>contact</div>
+			<div className="contact">
+				<div className="contact-banner parallax">
+					<h1>Contact Us!</h1>
+				</div>
+				<div className="social">
+					<div>
+						<h2>Architecture Brick & Tile</h2>
+						<p className="list-item">
+							<span className="fa-stack fa-lg">
+							    <i className="fa fa-circle fa-stack-2x"></i>
+							    <i className="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+							</span>
+							123 SOMEWHERE LANE
+						</p>
+
+						<p className="list-item">
+							<span className="fa-stack fa-lg">
+							    <i className="fa fa-circle fa-stack-2x"></i>
+							    <i className="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+							</span>
+							Indianapolis, IN 46222
+						</p>
+
+						<p className="list-item">
+							<span className="fa-stack fa-lg">
+							    <i className="fa fa-circle fa-stack-2x"></i>
+							    <i className="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+							</span>
+							abt@abtinfo.com
+						</p>
+
+						<h2>Social</h2>
+
+						<p className="list-item">
+							<a href="#">
+								<span className="fa-stack fa-lg">
+								    <i className="fa fa-circle fa-stack-2x"></i>
+								    <i className="fa fa-instagram fa-stack-1x fa-inverse"></i>
+								</span>
+								instagram-username
+							</a>
+						</p>
+
+						<p className="list-item">
+							<a href="#">
+								<span className="fa-stack fa-lg">
+								    <i className="fa fa-circle fa-stack-2x"></i>
+								    <i className="fa fa-houzz fa-stack-1x fa-inverse"></i>
+								</span>
+								houzz-username
+							</a>
+						</p>
+
+						<p className="list-item">
+							<a href="#">
+								<span className="fa-stack fa-lg">
+								    <i className="fa fa-circle fa-stack-2x"></i>
+								    <i className="fa fa-youtube fa-stack-1x fa-inverse"></i>
+								</span>
+								youtube-username
+							</a>
+						</p>
+
+					</div>
+				</div>
+				<div className="contact-form">
+					<div>
+						<h2>Get ahold of us!</h2>
+						<form>
+							<div className="cf-email"><input name="" placeholder="Your e-mail" title="Your e-mail" type="text" /></div>
+	                        <div className="cf-name"><input name="" placeholder="Your name" title="Your name" type="text" /></div>
+	                        <div className="cf-message"><textarea placeholder="Your massage" name="" rows="5"></textarea></div>
+	                        <div className="cf-submit"><button type="submit">Submit Button</button></div>
+						</form>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
