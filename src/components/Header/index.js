bbe54bcac891edbf540f44ee043018ffb0991df4
JT <jmsMaupin1@yahoo.com>
2017-08-03 20:57:53 -0400

    Fix image issues, make navbar close on click

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import { Link } from 'react-router';
 import './header.css';
 
@@ -18,6 +19,10 @@ export default class Header extends Component {
 		}});
 	}
 
+	click(e){
+		let nav = $('.nav-button').click();
+	}
+
 	render() {
 		let classes = this.props.isSticky? "contracted" : "expanded";
 		let revealed = this.state.revealed? "revealed" : "concealed";
@@ -25,11 +30,11 @@ export default class Header extends Component {
 			<div className={"navigation " + classes}>
 				<div className="fluid-container">
 					<div className='navigation-content col-lg-12'>
-							<Link className={'col-lg-3 ' + revealed} to="/">Home</Link>
-							<Link className={'col-lg-3 ' + revealed} to="/about">About</Link>
-							<Link className={'col-lg-3 ' + revealed} to="/"><img alt="ABT Logo" className="logo" src={ABTIcon}></img></Link>
-							<Link className={'col-lg-3 ' + revealed} to="/suppliers">Suppliers</Link>
-							<Link className={'col-lg-3 ' + revealed} to="/contact">Contact Us</Link>
+							<Link onClick={this.click.bind(this)} className={'col-lg-3 ' + revealed} to="/">Home</Link>
+							<Link onClick={this.click.bind(this)} className={'col-lg-3 ' + revealed} to="/about">About</Link>
+							<Link onClick={this.click.bind(this)} className={'col-lg-3 ' + revealed} to="/"><img alt="ABT Logo" className="logo" src={ABTIcon}></img></Link>
+							<Link onClick={this.click.bind(this)} className={'col-lg-3 ' + revealed} to="/suppliers">Suppliers</Link>
+							<Link onClick={this.click.bind(this)} className={'col-lg-3 ' + revealed} to="/contact">Contact Us</Link>
 						<button className="nav-button" onClick={this.handleClick.bind(this)}><i className="fa fa-bars"/></button>
 					</div>
 				</div>
---
+++ src/views/About/index2(1).js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class  extends Component {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
